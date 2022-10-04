efb6fd99e3c2ed28c1d36b988a00948fa90a7d6b
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-10-04 19:37:39 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent {  
   title = 'project2';
+  username: string = "";
+  password: string = "";
+  msg: string = "";
+
+  CheckLogin(txt1:any) {
+    if(this.username==="Admin" && this.password==="Admin123"){
+      this.msg = "Successful Login";
+    }else{
+      this.msg = "Invalid Login";
+      txt1.focus();
+    }
+  }
 }
